2e43d834d81deee45d59be12302ab0a0cea4fd62
Eleisabet <6175056@gmail.com>
2024-10-05 21:54:15 -0700

    add homework

--- Math-Trainer/Math-Trainer/TrainViewController.swift
+++ Math-Trainer/Math-Trainer/TrainViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 final class TrainViewController: UIViewController {
+    
+    
+    @IBOutlet var buttonsTwoCollection: [UIButton]!
+    
     // MARK: - Properties
     var type: MathTypes = .add {
         didSet {
             print(type)
         }
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureTwoButtons()
+    }
+    
+    private func configureTwoButtons() {
+        // Add shadow
+        buttonsTwoCollection.forEach {
+            button in button.layer.shadowColor = UIColor.darkGray.cgColor
+            button.layer.shadowOffset = CGSize(width: 0, height: 2)
+            button.layer.shadowOpacity = 0.4
+            button.layer.shadowRadius = 3
+        }
+    }
+    
+    
 }
 
