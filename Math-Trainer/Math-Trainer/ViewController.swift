5d616e3a7cb32423cba37b5dd60f71b6718cc020
Eleisabet <6175056@gmail.com>
2024-10-14 18:48:29 -0700

    Dasha's lecture

--- Math-Trainer/Math-Trainer/ViewController.swift
+++ Math-Trainer/Math-Trainer/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
     @IBAction func buttonsAction(_ sender: UIButton) {
         selectedType = MathTypes(rawValue: sender.tag) ?? .add
         performSegue(withIdentifier: "goToNext", sender: sender)
-       
+        
     }
     
     @IBAction func unwindAction(unwindSegue: UIStoryboardSegue) { }
